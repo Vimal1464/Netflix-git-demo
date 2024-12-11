4f1e664d328a504ab92ddb248798a4a0dca0dc46
Vimal Pandey <vimalpandey1978.78@gmail.com>
2024-12-11 14:39:57 +0530

    PR comments

--- Login.js
+++ Login.js
@@ -1,12 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Login = () => {
+export default function Login() {
   return (
     <View>
       <Text>Login</Text>
     </View>
   )
 }
-
-export default Login
